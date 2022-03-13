6c8644a68509609b2287226df6e8f7b4dbe8f347
sunadoi <y.sunadoi@gmail.com>
2022-03-13 20:58:36 +0900

    main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "terraform-sample" {
   ami           = "ami-0701e21c502689c31"
-  instance_type = "tc2.micro"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "hello"
+  }
 }
 
 provider "aws" {
